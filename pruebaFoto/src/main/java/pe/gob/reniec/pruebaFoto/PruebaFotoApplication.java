9138656e5000fb72301884f4c39b5efeb93a9f32
Karen Huaisara Olarte <khuaisara@reniecperu.net>
2020-09-11 12:46:06 -0500

    prueba1

---
+++ pruebaFoto/src/main/java/pe/gob/reniec/pruebaFoto/PruebaFotoApplication.java
@@ -0,0 +1,13 @@
+package pe.gob.reniec.pruebaFoto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaFotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaFotoApplication.class, args);
+	}
+
+}
---
+++ pruebaFoto/src/test/java/pe/gob/reniec/pruebaFoto/PruebaFotoApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.gob.reniec.pruebaFoto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaFotoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
